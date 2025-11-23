bd2b108b1b53a671736b99b4c98d37514b39f629
Alyona <alenasaun@mail.ru>
2025-11-23 18:01:06 +0500

    fghj

--- scripts/script.js
+++ scripts/script.js
@@ -32,22 +32,14 @@ function setTheme(theme) {
   localStorage.setItem('theme', theme);
 }
 
-function setActiveButton(buttonsArray, theme) {
-  buttonsArray.forEach((button) => {
+function setActiveButton(buttons, theme) {
+  buttons.forEach(button => {
     button.classList.remove('header__theme-menu-button_active');
-    button.removeAttribute('disabled');
+    const buttonTheme = [...button.classList]
+      .find((cn) => cn.includes('_type_'))
+      ?.split('_type_')[1];
+    if (buttonTheme === theme) {
+      button.classList.add('header__theme-menu-button_active');
+    }
   });
-  const target = buttonsArray.find((button) =>
-    button.classList.contains(`header__theme-menu-button_type_${theme}`)
-  );
-  if (target) {
-    target.classList.add('header__theme-menu-button_active');
-    target.setAttribute('disabled', true);
-  } else {
-    const autoButton = document.querySelector(
-      '.header__theme-menu-button_type_auto'
-    );
-    autoButton.classList.add('header__theme-menu-button_active');
-    autoButton.setAttribute('disabled', true);
-  }
 }
